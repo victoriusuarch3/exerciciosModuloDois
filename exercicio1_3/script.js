23406273d4d3f81006e1286fa10a8671f8df8852
Víctor Vitoriano <victorfrance20@yahoo.com>
2022-11-09 23:50:01 -0300

    Exercicio 1.3

---
+++ exercicio1_3/script.js
@@ -0,0 +1,33 @@
+//Estrutura condicional. Exercicio 1.
+// se y(1) divido por y(1) for igual a y(1)
+var y = 1;
+var x = 2;
+
+switch (1) {
+    case (resultado1 = y/1 == 2):
+        break;
+    case (resultado1 = y/1 == 1):
+        break;
+        case (resultado2 = y/2 == 2):
+        break;
+    case (resultado2 = y/2 == 1):
+        break;
+}
+console.log(resultado1="Verdeiro");
+console.log(resultado2="Correto");
+
+
+
+
+
+
+
+//Estrutura condicional. Exercicio 2.
+
+
+var hora = 1;
+var minutos = 0;
+
+var resultado = hora > 0 ? minutos = hora * 60 : minutos = 60;
+
+//console.log(resultado)
