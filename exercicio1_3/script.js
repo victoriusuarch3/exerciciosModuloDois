9d9ffa2a1a16f8505266d05c199c3ff0f98be622
Víctor Vitoriano <victorfrance20@yahoo.com>
2022-11-10 00:44:41 -0300

    Exercicio 1.3, tive um problema na lógica e decidi reformular.

--- exercicio1_3/script.js
+++ exercicio1_3/script.js
@@ -1,34 +1,33 @@
-//Estrutura condicional. Exercicio 1.
+console.log("Estrutura condicional. Exercicio 1. (2 página)")
 // se y(1) divido por y(1) for igual a y(1)
 var y = 1;
 var x = 2;
 
-switch (y) {
-    case console.log("Quanto é 2/1? =", resultado1 = x / 1, ", então é um numero primo"):
+switch (x) {
+    case 1: (console.log("Quanto é 2/1 =", x / y, ", então é um numero primo"))
         break;
-    case console.log("Quanto é 2/2:", resultado1 = x/x,"então é um numero primo"):
+    case 2: (console.log("Quanto é 2/1 =", x / y, ", então é um numero primo"))
         break;
 };
+switch (x) {
+    case 1: console.log("Quanto é 2/2:", x / x, ", então é um numero primo")
+        break;
+    case 2: console.log("Quanto é 2/2:", x / x, ", então é um numero primo")
+        break;
+}
+
 console.log("Um número primo tem como caracteristica ser divisível por si proprio ou por 1")
 
-
-
-
-
-//console.log(resultado3 = "Correto");
-
-
-
-
-
-
+console.log("⠀⠀⠀⠀⠀⠀⠀⠀⠀")
+console.log("⠀⠀⠀⠀⠀⠀⠀⠀⠀")
+console.log("⠀⠀⠀⠀⠀⠀⠀⠀⠀")
 
 //Estrutura condicional. Exercicio 2.
 
-
+console.log("Estrutura condicional. Exercicio 2. (2 página)")
 var hora = 1;
 var minutos = 0;
 
 var resultado = hora > 0 ? minutos = hora * 60 : minutos = 60;
 
-//console.log(resultado)
+console.log("resultado ", resultado)
