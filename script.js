cdf22bb703e94daa2ca68d7221d5a6cb1ff8acaa
Víctor Vitoriano <victorfrance20@yahoo.com>
2022-11-09 22:07:41 -0300

    exercicio operadores lógicos

---
+++ script.js
@@ -0,0 +1,11 @@
+if(bebo_agua&&suco){console.log("minha_sede_termina")}
+
+
+//Se Caminho 5km e corro 2km, passo o dia bem.
+if(caminho_5km&&corro_2km){console.log("passo_o_dia_bem")}
+
+//Se levanto cedo OU antes das 8h, saio tranquilo.
+if(levanto_cedo||antes_das_8h){console.log("saio_tranquilo")}
+
+//SE NÃO termino o trablho OU NÃO tenho compromisso, saio tarde.
+if(!termino_o_trabalho||!tenho_compromisso){("saio_trabalho")}
