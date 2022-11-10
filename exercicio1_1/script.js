52ecfd8c7c1ed9735def751828c3b10619c84fd6
Víctor Vitoriano <victorfrance20@yahoo.com>
2022-11-09 21:08:28 -0300

    Exercicios Operadores aritméticos

---
+++ exercicio1_1/script.js
@@ -0,0 +1,21 @@
+
+        var num1 = 22;
+        var num2 = 20;
+        var soma = num1 + num2;
+        var subtracao = num1 - num2;
+
+
+        var num3 = 80;
+        var num4 = 3;
+
+        var divisao = num3 / num4;
+        var modulo = num3 % num4;
+
+        console.log("Resultado da Soma: ",  soma);
+        console.log("Resultado da Subtração: ", subtracao);
+        console.log("Resultado da divisão: ", divisao);
+        console.log("Resultado do resto inteiro da divisão: ", modulo);
+        console.log("Resultado da Soma, somando 5 a variavel soma: ",  soma + 5);
+        console.log("Resultado da Soma, subtraindo 3 a variavel soma: ",  soma - 3);
+
+
