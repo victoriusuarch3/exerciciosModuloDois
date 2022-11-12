91b35ada57fd95766d34fdfa1f723b66880cb280
Víctor Vitoriano <victorfrance20@yahoo.com>
2022-11-11 23:19:42 -0300

    soma com while e do/while

--- prática2.1.1/script.js
+++ prática2.1.1/script.js
@@ -5,24 +5,39 @@
 
 var list = [12, 6, 9, 42, 11, 102, 44, 15];
 var total = 0;
-for (var i = 0; i < list.length; total = total + list[i++]) {  
+for (var i = 0; i < list.length; total = total + list[i++]) {
 }
-console.log(total);
+console.log("Soma de todos os números com /FOR: ", total);
 
 /*     faça a soma de todos os numeros usando:
        12, 6, 9, 42, 11, 102, 44, 15     
        
        WHILE                     */
 
+var soma = 0;
+var array = [12, 6, 9, 42, 11, 102, 44, 15]
+var quantidade = array.length;
+var totalX = 0;
 
-
-
-
-
-
+while (quantidade--) {
+   totalX += array[quantidade];
+}
+console.log("Soma de todos os números com /WHILE: ", totalX);
 
 
 /*     faça a soma de todos os numeros usando:
        12, 6, 9, 42, 11, 102, 44, 15     
        
        DO WHILE                    */
+
+var soma = 0;
+var array = [12, 6, 9, 42, 11, 102, 44, 15]
+var quantidade = array.length-1;
+var totalY = 0;
+
+do {
+   totalY += array[quantidade];
+   
+}
+while (quantidade--)
+console.log("Soma de todos os números com DO/WHILE: ", totalY);
