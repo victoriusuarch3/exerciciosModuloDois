399607fa145589c97b95da249f8094326f8faf0f
Víctor Vitoriano <victorfrance20@yahoo.com>
2022-11-14 22:50:48 -0300

    Comparando username e password inseridas

--- prática2.1.1/script.js
+++ prática2.1.1/script.js
@@ -20,7 +20,7 @@ var quantidade = array.length;
 var totalX = 0;
 
 while (quantidade--) {
-   totalX += array[quantidade];
+       totalX += array[quantidade];
 }
 console.log("Soma de todos os números com /WHILE: ", totalX);
 
@@ -32,12 +32,12 @@ console.log("Soma de todos os números com /WHILE: ", totalX);
 
 var soma = 0;
 var array = [12, 6, 9, 42, 11, 102, 44, 15]
-var quantidade = array.length-1;
+var quantidade = array.length - 1;
 var totalY = 0;
 
 do {
-   totalY += array[quantidade];
-   
+       totalY += array[quantidade];
+
 }
 while (quantidade--)
 console.log("Soma de todos os números com DO/WHILE: ", totalY);
@@ -49,27 +49,39 @@ console.log("Números menores que 40:  ");
 var num = 1;
 
 function numeros(num) {
-       for (var num = 1; num < 40; divisor++) 
-       
-       return true;
-   }
-   
-   var determinadoNumero = 40;
-   
-   for (var i = num; i < determinadoNumero; i++) if (numeros(i)) console.log(i);
+       for (var num = 1; num < 40; divisor++)
 
-   /*  Números mairoes que 50  */
+              return true;
+}
 
-   console.log("Números mairoes que 50:  ");
+var determinadoNumero = 40;
+
+for (var i = num; i < determinadoNumero; i++) if (numeros(i)) console.log(i);
+
+/*  Números mairoes que 50  */
+
+console.log("Números mairoes que 50:  ");
 
 var num2 = 51;
 
 function numeros(num) {
-       for (var num2 = 51; num2 > 50; divisor++) 
-       
-       return true;
-   }
-   
-   var determinadoNumero = 100;
-   
-   for (var i = num2; i < determinadoNumero; i++) if (numeros(i)) console.log(i);
+       for (var num2 = 51; num2 > 50; divisor++)
+
+              return true;
+}
+
+var determinadoNumero = 100;
+
+for (var i = num2; i < determinadoNumero; i++) if (numeros(i)) console.log(i);
+
+// parar quando encontrar o pruimeiro número primo
+
+console.log("Parar quando encontrar o primeiro número primo")
+
+var firstnum = 3
+var lastnum = 10
+
+while (firstnum < lastnum) {
+       firstnum % 1 === firstnum && firstnum % firstnum === 1 ? console.log('O número não é primo') : console.log('O número',(firstnum),' é primo')
+       break;
+}
---
+++ prática2.1.5/script.js
@@ -0,0 +1,21 @@
+var userName = parseInt(prompt("Insira seu Usuário: "))
+var passWord = parseInt(prompt("Insira sua Senha: "))
+
+
+if(prompt("Insira seu usuário novamente: ") == userName) document.write("Seu nome está correto");
+
+
+if(prompt("Insira sua Senha novamente: ") == passWord) document.write("Sua senha está correta");
+
+
+
+
+
+//if  (compsenha = userName && userName2)
+//console.log("Seu nome está correto", compsenha)
+
+
+
+
+//if  (passWord && passWord2)
+//console.log("Sua senha está correta", passWord)
