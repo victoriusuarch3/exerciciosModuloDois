98c06bd261d36f8be42c9b035f7d2551efd35c7b
Víctor Vitoriano <victorfrance20@yahoo.com>
2022-11-14 21:23:15 -0300

    Numeros menores que 40, e números maiores que 50

--- prática2.1.1/script.js
+++ prática2.1.1/script.js
@@ -41,3 +41,35 @@ do {
 }
 while (quantidade--)
 console.log("Soma de todos os números com DO/WHILE: ", totalY);
+
+/*  Números menores que 40  */
+
+console.log("Números menores que 40:  ");
+
+var num = 1;
+
+function numeros(num) {
+       for (var num = 1; num < 40; divisor++) 
+       
+       return true;
+   }
+   
+   var determinadoNumero = 40;
+   
+   for (var i = num; i < determinadoNumero; i++) if (numeros(i)) console.log(i);
+
+   /*  Números mairoes que 50  */
+
+   console.log("Números mairoes que 50:  ");
+
+var num2 = 51;
+
+function numeros(num) {
+       for (var num2 = 51; num2 > 50; divisor++) 
+       
+       return true;
+   }
+   
+   var determinadoNumero = 100;
+   
+   for (var i = num2; i < determinadoNumero; i++) if (numeros(i)) console.log(i);
