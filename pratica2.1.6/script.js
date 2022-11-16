cdada7cbd3013bb0de858fe6492f5d35d2637738
Víctor Vitoriano <victorfrance20@yahoo.com>
2022-11-15 23:13:34 -0300

    Trocando VAR por LET

--- pratica2.1.6/script.js
+++ pratica2.1.6/script.js
@@ -1,3 +1,5 @@
+// corrigindo erro prática 2.1.6 desafio 1° página
+
 function confirmarDados(nome){
 var nomeCompleto = nome + ' da Silva';
 var idade = 20;
@@ -8,5 +10,26 @@ function getAnoNascimento() {
 console.log('Nome completo:', nomeCompleto);
 console.log('Ano de nascimento:', getAnoNascimento());
 console.log('Sua idade é:', idade);
+}
+confirmarDados('Marcos');
+
+console.log('///////////////////////////////////////////////////////////');
+// Refatore o Código, trocando a palavra reservada de VAR para LET e CONST
+
+function confirmarDados(nome){
+    let nomeCompleto = nome + ' da Silva';
+    function getAnoNascimento(){
+        var idade = 40;
+        var ano = 2022 - idade;
+        return ano;
+    }
+    var anoDeNascimento = getAnoNascimento();
+    let geracao = 'Geração X';
+    if(anoDeNascimento <= 1995){
+        
+    }
+    console.log('Nome Completo: ', nomeCompleto);
+    console.log('Ano de Nascimento: ', anoDeNascimento);
+    console.log('Geração: ', geracao);
 }
 confirmarDados('Marcos');
